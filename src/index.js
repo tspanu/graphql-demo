d9931b2f0e7fdc705285af190a024e40f44d5a81
Travis <tjspanu@gmail.com>
2019-01-22 09:29:51 -0600

    Updated server running message

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import '@babel/polyfill'
 import server from './server'
 
 server.start({ port: process.env.PORT || 4000 }, () => {
-    console.log('The server is up!')
+    console.log('Server is running on http://localhost:4000')
 })
